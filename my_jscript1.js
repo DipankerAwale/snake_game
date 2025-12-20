2d723914a51ab2084b2cbc7d9b40043e9242f3a3
DipankerAwale <133489531+DipankerAwale@users.noreply.github.com>
2025-12-20 22:24:34 +0000

    third update

--- my_jscript1.js
+++ my_jscript1.js
@@ -1,6 +1,7 @@
 console.log("first print statement");
 
 const grid=[];
+const grid_energy=[];
 for(let y=0; y<10;y++){
     const row=[];
     for(let x=0; x<10;x++){
@@ -13,6 +14,7 @@ for (let i = 0; i < 5; i++) {
     const randomNumber1 = Math.floor(Math.random() * (10 - 0)) + 0;
     const randomNumber2 = Math.floor(Math.random() * (10 - 0)) + 0;
     (grid[randomNumber1][randomNumber2]="X");
+    grid_energy.push([randomNumber1, randomNumber2]);
 }
 
 for (let y = 0; y < 10; y++) {
@@ -23,3 +25,4 @@ for (let y = 0; y < 10; y++) {
     console.log(line);
 }
 
+console.log("Energy cells at positions: ", grid_energy);
